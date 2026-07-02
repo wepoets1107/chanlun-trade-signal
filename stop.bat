9810916069ee7ed9d85b6ee39afbf1494a9c7d6c
wepoets1107 <zhangbojin1107@gmail.com>
2026-07-02 22:30:43 +0800

    add stop.bat

---
+++ stop.bat
@@ -0,0 +1,2 @@
+@echo off
+powershell -NoProfile -ExecutionPolicy Bypass -Command "$pids = Get-NetTCPConnection -LocalPort 8040 -State Listen -ErrorAction SilentlyContinue | Where-Object { $_.OwningProcess -ne 0 } | Select-Object -ExpandProperty OwningProcess -Unique; foreach ($processId in $pids) { Stop-Process -Id $processId -Force; Write-Host \"Stopped process $processId\" }"
